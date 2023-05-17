5432ccf24f541dce5e67b53a89e4f4ad8ecbe0d9
layken varholdt <laykenv@gmail.com>
2023-05-17 18:07:32 -0500

    lots of work on interview page

--- src/pages/interview.tsx
+++ src/pages/interview.tsx
@@ -1,13 +1,68 @@
 import Box from "@mui/material/Box";
 import Stack from "@mui/material/Stack";
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import { interviewContext } from "../interviewContext";
 import { reactQuestions, angularQuestions, behavioralQuestions } from "../data/interviewQuestions";
-import { Button } from "@mui/material";
+import { Button, TextField } from "@mui/material";
+import { Formik, Form, FormikHelpers, useFormik } from "formik";
+import * as yup from 'yup';
+import { useNavigate } from "react-router-dom";
+import TypewriterComponent from "typewriter-effect";
+
+
+interface Values {
+    answer: string;
+}
+
+const validationSchema = yup.object({
+    answer: yup.string().max(100, "answer should be 100 characters max").required("Answer is Required")
+});
 
 
 const Interview = () => {
-    const { interviewLevel, interviewJob, setQuestions, setAnswers} = useContext(interviewContext);
+    const { interviewLevel, interviewJob, questions, setQuestions, setAnswers, answers} = useContext(interviewContext);
+    const [questionNumber, setQuestionNumber] = useState(0 as number);
+    const navigate = useNavigate();
+    const synthesis = window.speechSynthesis;
+    const recognition = new webkitSpeechRecognition();
+
+    useEffect(() => {
+        const utterance = new SpeechSynthesisUtterance(questions[questionNumber]);
+        const voice = synthesis.getVoices().filter(function (voice) {
+            return voice.lang === 'en';
+          })[0];
+        utterance.voice = voice;
+        utterance.pitch = 1.5;
+        utterance.rate = 1.25;
+        utterance.volume = 0.8;
+        synthesis.speak(utterance);
+    }, [questionNumber]);
+
+    useEffect(() => {
+        const utterance = new SpeechSynthesisUtterance(questions[0]);
+        const voice = synthesis.getVoices().filter(function (voice) {
+            return voice.lang === 'en';
+          })[0];
+        utterance.voice = voice;
+        utterance.pitch = 1.5;
+        utterance.rate = 1;
+        utterance.volume = 0.8;
+        synthesis.speak(utterance);
+    }, [questions]);
+
+    const formik = useFormik({
+        initialValues: {
+            answer: "",
+        },
+        validationSchema: validationSchema,
+        onSubmit: (values: Values) => {
+            console.log(values.answer);
+            const newAnswers = [...answers, values.answer]
+            setAnswers(newAnswers);
+            setQuestionNumber(prevState => prevState + 1);
+            formik.resetForm();
+        },
+    });
 
     useEffect(() => {
         const questions : string[] = [];
@@ -17,19 +72,36 @@ const Interview = () => {
             questions.push(shuffledQuestions[0], shuffledQuestions[1], shuffledQuestions[2], shuffledQuestions[3]);
             setQuestions(questions);
             console.log(questions);
-            
         }
-    }, [interviewJob]);
+    }, [interviewJob]); 
+
+    useEffect(() => {
+        if (questionNumber > 4) {
+            navigate("/interviewAnalysis");
+        }
+    }, [questionNumber]);
     
     return(
         <Stack>
-            <Box>{interviewLevel} {interviewJob} Interview</Box>
-            <Box>Question</Box>
-            <Box>Answer</Box>
+            <Stack>
+                <Box>{interviewLevel} {interviewJob} Interview</Box>
+                <Box>Question {questionNumber + 1} / 5</Box>
+            </Stack>
+            <Box>
+               { questions[1] && questionNumber === 0 ? <TypewriterComponent onInit={(typewriter) => {typewriter.changeDelay(40).typeString(questions[questionNumber]).start()}}/> : null }
+               { questions[1] && questionNumber === 1 ? <TypewriterComponent onInit={(typewriter) => {typewriter.changeDelay(40).typeString(questions[questionNumber]).start()}}/> : null } 
+               { questions[1] && questionNumber === 2 ? <TypewriterComponent onInit={(typewriter) => {typewriter.changeDelay(40).typeString(questions[questionNumber]).start()}}/> : null } 
+               { questions[1] && questionNumber === 3 ? <TypewriterComponent onInit={(typewriter) => {typewriter.changeDelay(40).typeString(questions[questionNumber]).start()}}/> : null } 
+               { questions[1] && questionNumber === 4 ? <TypewriterComponent onInit={(typewriter) => {typewriter.changeDelay(40).typeString(questions[questionNumber]).start()}}/> : null } 
+ 
+            </Box>
+            <form onSubmit={formik.handleSubmit}>
+            <TextField id="answer" name="answer" label="answer" value={formik.values.answer} onChange={formik.handleChange} error={formik.touched.answer && Boolean(formik.errors.answer)} helperText={formik.touched.answer && formik.errors.answer}>Answer</TextField>
             <Stack>
                 <Box>Microphone</Box>
-                <Button>Next Question / Finish Interview </Button>
+                <Button type="submit"> Next Question / Finish Interview </Button>
             </Stack>
+            </form>
         </Stack>
     )
 };
--- src/pages/postInterview.tsx
+++ src/pages/postInterview.tsx
@@ -1,6 +1,16 @@
+import { useEffect, useContext } from "react";
+import { interviewContext } from "../interviewContext";
+
 
 
 const PostInterview = () => {
+    const {  questions,  answers} = useContext(interviewContext);
+
+    useEffect(() => {
+        console.log(questions, answers);
+        
+    }, []);
+
     return(
         <div>Post Interview</div>
     )
